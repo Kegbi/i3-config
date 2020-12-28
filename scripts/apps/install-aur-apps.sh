00839ec554be3c42157aa6a786f20406a362042c
Kegbi <kegbi.networking@gmail.com>
2020-12-28 04:31:53 +0300

    Changed i3bar to i3blocks

--- scripts/apps/install-aur-apps.sh
+++ scripts/apps/install-aur-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pikaur -Syu --noconfirm cherrytree vmware-workstation keepnote rtl8812au422-dkms-git vscodium-bin libpdfium-nojs megasync wps-office clion clion-jre webstorm webstorm-jre pycharm-professional postman-bin kite timeset 
+pikaur -Syu --noconfirm cherrytree vmware-workstation keepnote rtl8812au422-dkms-git vscodium-bin libpdfium-nojs megasync wps-office clion clion-jre webstorm webstorm-jre pycharm-professional postman-bin kite timeset yaourt nvim
